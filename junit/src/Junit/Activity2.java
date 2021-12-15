56b573d9a5bee5bb134537f7e1bd53485d0fb4bc
Rajiv Ranjan6 <rajivranjan@in.ibm.com>
2021-12-15 10:40:18 +0530

    JUnit Activity code

---
+++ junit/src/Junit/Activity2.java
@@ -0,0 +1,24 @@
+package Junit;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class Activity2 {
+
+	@Test
+	void notEnoughFunds() {
+	    // Create an object for BankAccount class
+	    BankAccount account = new BankAccount(9);
+
+	    // Assertion for exception
+	    assertThrows(NotEnoughFundsException.class, () -> account.withdraw(10));
+	}
+	@Test
+	void enoughFunds() {
+	    // Create an object for BankAccount class
+	    BankAccount account = new BankAccount(100);
+
+	    // Assertion for no exceptions
+	    assertDoesNotThrow(() -> account.withdraw(100));
+	}
+}
